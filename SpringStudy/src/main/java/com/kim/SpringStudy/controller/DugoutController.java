637df491ab82db624576d89c8cf8b6067df85f7b
SungChul23 <kimsam0923@naver.com>
2025-08-17 20:18:06 +0900

    [더그아웃]250817 -1

--- SpringStudy/src/main/java/com/kim/SpringStudy/controller/DugoutController.java
+++ SpringStudy/src/main/java/com/kim/SpringStudy/controller/DugoutController.java
@@ -375,4 +375,43 @@ public class DugoutController {
 
     }
 
+    //야구 룰 설명
+    @GetMapping("/rules")
+    public String showRules() {
+        return "baseballRules/rules";
+    }
+    @GetMapping("/rules/quickstart")
+    public String showQuickstart() {
+        return "baseballRules/quickstart";
+    }
+    @GetMapping("/rules/offense")
+    public String showOffense() {
+        return "baseballRules/offense";
+    }
+    @GetMapping("/rules/defense")
+    public String showDefense() {
+        return "baseballRules/defense";
+    }
+    @GetMapping("/rules/baserun")
+    public String showBaserun() {
+        return "baseballRules/baserun";
+    }
+    @GetMapping("/rules/pitching")
+    public String showPitching() {
+        return "baseballRules/pitching";
+    }
+    @GetMapping("/rules/calls")
+    public String showCalls() {
+        return "baseballRules/calls";
+    }
+    @GetMapping("/rules/kbo")
+    public String showKBO() {
+        return "baseballRules/kbo";
+    }
+    @GetMapping("/rules/mis")
+    public String showMis() {
+        return "baseballRules/mis";
+    }
+
+
 }
